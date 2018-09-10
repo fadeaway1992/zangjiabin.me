b4f1e35875eff3a7ac23b485ae0339c88d668ce9
臧甲彬 <fadeaway@zangjiabindeMacBook-Pro.local>
2018-09-10 23:21:32 +0800

    test

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require('path')
 
 /* GET admin listing. */
-router.get('/', function(req, res, next) {
+router.get(/.*/, function(req, res, next) {
   res.sendFile(path.join(__dirname, '../admin/dist/index.html'));
 });
 
