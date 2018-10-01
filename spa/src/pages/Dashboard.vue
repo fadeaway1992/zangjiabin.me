c27014878719ecb90d4ec2554539faf877145083
fadeaway <498497303@qq.com>
2018-10-02 00:21:12 +0800

    完善主页的一些样式

--- spa/src/pages/Dashboard.vue
+++ spa/src/pages/Dashboard.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <div v-if="posts.length" v-for="post in posts" :key="post.id">
-      <h2>{{post.title}}</h2>
-      <div v-html="markdown().toHTML(post.body)"></div>
+  <div class="dashboard-page page-container">
+    <!-- 文章 -->
+    <div class="posts-container">
+      <!-- 一篇文章 -->
+      <div class="post-container" v-if="posts.length" v-for="post in posts" :key="post.id">
+        <h2 class="post-title"><a :href="'/posts/' + post.id">{{post.title}}</a></h2><!-- 标题 -->
+        <div class="post-body" v-html="markdown().toHTML(post.body)"></div><!-- 正文 -->
+      </div>
     </div>
   </div>
 </template>
@@ -34,3 +38,14 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.dashboard-page.page-container {
+  .post-title {
+    color: #262626;
+    a:hover {
+      color: #57ad68;
+    }
+  }
+}
+</style>
