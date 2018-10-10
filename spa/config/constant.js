70293cfbde7eb93577589818b687606acdbbe0d7
fadeaway <498497303@qq.com>
2018-10-10 14:40:23 +0800

    适应手机屏幕

--- spa/config/constant.js
+++ spa/config/constant.js
@@ -4,3 +4,4 @@ export const ENVIRONMENT = process.env.NODE_ENV
 // export const httpUriPrefix = ENVIRONMENT === 'development' ? 'http://localhost:3000/api/v1' : 'https://zangjiabin.me/api/v1'
 
 export const httpUriPrefix = 'http://localhost:3000/api/v1'
+// export const httpUriPrefix = 'http://192.168.8.143:3000/api/v1'
