bf4e4b5af3e357d7d0af05dce29ee20a96410bc5
fadeaway <498497303@qq.com>
2018-10-11 20:15:29 +0800

    add index page

---
+++ spa/src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page-container index-page flex-row-left">
+    <!-- 侧栏 -->
+    <side-navi/>
+    <!-- 索引列表 -->
+    <div class="posts-container">
+      <ul class="post-list">
+
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideNavi from '@/components/SideNavi.vue'
+export default {
+  name: 'Index',
+  components: {
+    SideNavi
+  },
+}
+</script>
+
+<style lang="scss">
+.page-container.index-page {
+  width: 960px;
+  margin: 0 auto;
+  margin-top: 30px;
+}
+</style>
+
